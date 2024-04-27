4285e955cbc5adc794c677392d9590ef8e81ac21
Entrimonstack <163389442+Entrimonstack@users.noreply.github.com>
2024-04-28 01:25:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Showprofile from './Components/ShowProfile';
+
+function App() {
+    return (
+        <div>
+            <Showprofile/>
+        </div>
+    )
+}
+
+export default App;
